3b1bb8696353a9c0a59fabeffe34cbe20c24f650
yct21 <yct21@12tcy.com>
2022-04-10 18:27:00 +0800

    initialize

---
+++ task-1-feature/my-serde/src/lib.rs
@@ -0,0 +1,5 @@
+#[cfg(feature = "derive")]
+pub mod serde_derive;
+
+#[cfg(feature = "std")]
+pub mod serde_std;
---
+++ task-1-feature/src/main.rs
@@ -0,0 +1,7 @@
+use my_serde;
+
+fn main() {
+    println!("--task one--");
+    my_serde::serde_std::serde_std();
+    my_serde::serde_derive::serde_derive();
+}
